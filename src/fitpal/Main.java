68e765e0ed55eb749c7ee2ea809c3c8241a72471
tomy2311 <thomas.garcia@davinci.edu.ar>
2025-04-07 16:53:27 -0300

    a

--- src/fitpal/Main.java
+++ src/fitpal/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		JOptionPane.showMessageDialog(null,
-				"Hola soy antonio");
+				"Hola soy Tomy");
 		JOptionPane.showMessageDialog(null,
 				"creando mi rama");
 	}
