ccdba4ce0710db1cc977842f4a257d5cf64da76d
Anthony2080 <antonio.pirotta@davinci.edu.ar>
2025-04-07 16:41:26 -0300

    Update Main.java

--- src/fitpal/Main.java
+++ src/fitpal/Main.java
@@ -1,8 +1,12 @@
 package fitpal;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		JOptionPane.showMessageDialog(null,
+				"Hola soy antonio");
 	}
 }
